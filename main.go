b0c20608526bc0daf3a62ad551b138ec73639998
Justin Graham <justin.m.graham2@gmail.com>
2026-03-20 17:30:21 -0400

    refactored server as a global for easier endpoint management

--- main.go
+++ main.go
@@ -16,33 +16,46 @@ import (
 	_ "github.com/J-Graham22/BudgetBuddyServer/src/db/repository"
 )
 
-func main() {
-	r := gin.Default()
+r := gin.Default()
 
+func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Welcome to Budget Buddy Server!",
 		})
 	})
 
-	//households
+	addCRUDEndpoints()
+
+	log.Println("Starting server on port :8080")
+	r.Run(":8080")
+}
+
+func addCRUDEndpoints() {
+	addHouseholdCRUD()
+	addUserCRUD()
+	addCategoryCRUD()
+	addTransactionCRUD()
+}
+
+func addHouseholdCRUD() {
 	// router.HandleFunc("POST /households/{name}", controllers.AddHousehold)
 	r.POST("/households/:name", controllers.AddHousehold)
+}
 
-	//users
+func addUserCRUD() {
 	// router.HandleFunc("POST /users", controllers.AddUser)
 	r.POST("/users", controllers.AddUser)
+}
 
-	//categories
+func addCategoryCRUD() {
 	// router.HandleFunc("POST /categories/{id}", controllers.AddCategory)
 	r.POST("/categories/:id", controllers.AddCategory)
 	// router.HandleFunc("GET /categories/{id}", controllers.GetCategoriesForHousehold)
 	r.GET("/categories/:id", controllers.GetCategoriesForHousehold)
+}
 
-	//transactions
+func addTransactionCRUD() {
 	// router.HandleFunc("GET /transactions/{id}", controllers.GetTransactionsForHousehold)
 	r.GET("/transactions/:id", controllers.GetTransactionsForHousehold)
-
-	log.Println("Starting server on port :8080")
-	r.Run(":8080")
 }
