f2bd83abd5971989998242fffe37a0455a4aa768
Justin Graham <justin.m.graham2@gmail.com>
2026-05-05 23:40:20 -0400

    started BE for onboarding flow

---
+++ src/controllers/onboardingController.go
@@ -0,0 +1,7 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddFromOnboarding(c *gin.Context) {
+	//TODO: add logic here for creating multiple records as part of onboarding flow
+}
